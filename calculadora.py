5d8cd4026fc700a3352bb6d398ad4af5ceb1f281
zx22student3214 <zx22student3214@colegiovalledelmiro.es>
2023-10-03 08:53:27 +0200

    calculadorav2

--- calculadora.py
+++ calculadora.py
@@ -3,6 +3,7 @@ x = int(input ("Introduce el primer numero: "))
 y = int(input ("Introduce el segundo numero: "))
 
 suma = x + y
+resta = x - y
 
 print("El resultado de la suma es: ", suma)
-
+print("El resultado de la resta es: ", resta)
