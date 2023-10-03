b48852e00ab3b5c6e351518b99330a5409e2ed4b
zx22student3214 <zx22student3214@colegiovalledelmiro.es>
2023-10-03 09:11:15 +0200

    division

--- calculadora.py
+++ calculadora.py
@@ -5,7 +5,9 @@ y = int(input ("Introduce el segundo numero: "))
 suma = x + y
 resta = x - y
 mutiplicacion = x * y
+division = x / y
 
 print("El resultado de la suma es: ", suma)
 print("El resultado de la resta es: ", resta)
 print("El resultado de la mutiplicacion es: ", mutiplicacion)
+print("El resultado de la division es: ", division)
